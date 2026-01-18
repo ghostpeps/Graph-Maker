5d4fe2dbcf932778dbe5cd1bef7c72937b0b002e
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-01-17 18:16:38 -0800

    Add matplotlib graph plotting to home.py

--- home.py
+++ home.py
@@ -1,1 +1,9 @@
+import matplotlib.pyplot as plt
 
+x_coords = [1, 2, 3, 4]
+y_coords = [1, 4, 2, 3]
+plt.plot(x_coords, y_coords)
+plt.xlabel('X-axis Label')
+plt.ylabel('Y-axis Label')
+plt.title('My First Python Graph')
+plt.show()
